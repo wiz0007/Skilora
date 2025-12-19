9be06bae396a24842c3d2abb2ec002cfc689751a
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-12-19 19:41:52 +0530

    scroll update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-
 import "./App.css";
 import Allroutes from "./routes/AllRoutes";
+import ScrollToTop from "./routes/ScrollToTop"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <ScrollToTop/>
       <Allroutes />
     </div>
   );
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/homePage/HomePage";
-import ScrollToTop from "./ScrollToTop";
+
 
 const Allroutes = () => {
   return (
     <Routes>
-      <ScrollToTop/>
       <Route path="/" element={<HomePage />} />
     </Routes>
   );
